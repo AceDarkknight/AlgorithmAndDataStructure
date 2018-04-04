963a8eaf0dbff3be810a648fa3b3b800d2d88447
Dilon Wu <acewudilin@foxmail.com>
2018-04-04 18:05:41 +0800

    add benchmark test

--- skipList/skipList_bench_test.go
+++ skipList/skipList_bench_test.go
@@ -1,1 +1,80 @@
 package skipList
+
+import (
+	"math/rand"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func BenchmarkSkipList_Insert_Ordered(b *testing.B) {
+	skipList := NewSkipList(32)
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		t := rand.New(rand.NewSource(time.Now().UnixNano())).Intn(100000)
+		_ = Hash([]byte(strconv.Itoa(t)))
+		skipList.Insert(uint64(i), i)
+	}
+}
+
+func BenchmarkSkipList_Insert_Randomly(b *testing.B) {
+	skipList := NewSkipList(32)
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		t := rand.New(rand.NewSource(time.Now().UnixNano())).Intn(100000)
+		index := Hash([]byte(strconv.Itoa(t)))
+		skipList.Insert(index, i)
+	}
+}
+
+func BenchmarkSkipList_Search_10000Elements(b *testing.B) {
+	skipList := NewSkipList(32)
+	for i := 0; i < 10000; i++ {
+		skipList.Insert(uint64(i), i)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		skipList.Search(uint64(i))
+	}
+}
+
+func BenchmarkSkipList_Search_100000Elements(b *testing.B) {
+	skipList := NewSkipList(32)
+	for i := 0; i < 100000; i++ {
+		skipList.Insert(uint64(i), i)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		skipList.Search(uint64(i))
+	}
+}
+
+func BenchmarkSkipList_Search_1000000Elements(b *testing.B) {
+	skipList := NewSkipList(32)
+	for i := 0; i < 1000000; i++ {
+		skipList.Insert(uint64(i), i)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		skipList.Search(uint64(i))
+	}
+}
+
+func BenchmarkSkipList_Search_10000000Elements(b *testing.B) {
+	skipList := NewSkipList(32)
+	for i := 0; i < 10000000; i++ {
+		skipList.Insert(uint64(i), i)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		skipList.Search(uint64(i))
+	}
+}
